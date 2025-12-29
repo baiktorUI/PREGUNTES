f0bdc9a147c5f544de0d2dafa5486b0ae6f38a61
Victor <baiktor@gmail.com>
2025-12-30 00:07:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
